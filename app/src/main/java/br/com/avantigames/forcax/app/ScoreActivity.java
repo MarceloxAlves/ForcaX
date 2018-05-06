c61d044c623a8830cfadd337a9baa6834db2a4ca
Marcelo <marceloalvessoft@gmail.com>
2018-05-06 02:45:07 -0300

    UI

---
+++ app/src/main/java/br/com/avantigames/forcax/app/ScoreActivity.java
@@ -0,0 +1,15 @@
+package br.com.avantigames.forcax.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.avantigames.forcax.R;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
